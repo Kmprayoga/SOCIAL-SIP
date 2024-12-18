31e49692f1330d0cbd15332c3ebdbce96829fb0d
dwiprayoga <komangyoga835@gmail.com>
2024-12-18 11:09:22 +0800

    Add qrcode url Railway deployment

--- qrCode.js
+++ qrCode.js
@@ -1,13 +1,13 @@
 const QRCode = require('qrcode');
 
-QRCode.toFile('qrcode1.png', 'http://localhost:3002/scan1', {
+QRCode.toFile('qrcode1.png', 'https://social-sip-production.up.railway.app/scan1', {
     errorCorrectionLevel: 'H',
 }, function (err) {
     if (err) throw err;
     console.log('QR Code 1 berhasil dibuat!');
 });
 
-QRCode.toFile('qrcode2.png', 'http://localhost:3002/scan2', {
+QRCode.toFile('qrcode2.png', 'https://social-sip-production.up.railway.app/scan2', {
     errorCorrectionLevel: 'H',
 }, function (err) {
     if (err) throw err;
